fd8b316648303172ac35d719cde999644115b49f
Tianxiao Zhao <tzh@kth.se>
2017-03-01 18:42:29 +0100

    Update flip.m

--- Hopfield-networks/info/flip.m
+++ Hopfield-networks/info/flip.m
@@ -1,4 +1,4 @@
-function y=flip(x,n)
-y=x;
-[dummy,index]=sort(rand(1,size(x,2)));
-y(index(1:n))=-y(index(1:n));
+function y = flip(x, n)
+y = x;
+[dummy, index] = sort(rand(1, size(x, 2)));
+y(index(1:n)) = -y(index(1:n));
