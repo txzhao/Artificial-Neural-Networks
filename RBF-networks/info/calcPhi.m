ba789f2ae9b9c9bf0cac7a7984ef86562e90764f
Tianxiao Zhao <tzh@kth.se>
2017-03-09 23:12:22 +0100

    Update calcPhi.m

--- RBF-networks/info/calcPhi.m
+++ RBF-networks/info/calcPhi.m
@@ -1,10 +1,10 @@
 % Calculate responses for gaussian RBF units from sample vector x
-function Phi=calcPhi(x,m,var);
+function Phi = calcPhi(x, m, var);
 
 %Dimension the function matrix fi
-Phi=zeros(rows(x),rows(m));
+Phi = zeros(rows(x), rows(m));
 
 %Calculate the RBF responses
-for i=1:rows(x)
-  Phi(i,:)=gauss(x(i,:),m,var)';
+for i = 1 : rows(x)
+  Phi(i, :) = gauss(x(i, :), m, var)';
 end
