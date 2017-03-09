50f524605dcbb8c5ef0559450cd1bdb4625146d8
Tianxiao Zhao <tzh@kth.se>
2017-03-09 23:15:31 +0100

    Update emiterb.m

--- RBF-networks/info/emiterb.m
+++ RBF-networks/info/emiterb.m
@@ -2,12 +2,12 @@
 
 diffm = 1;
 diffvar = 1;
-%plotbuffer(pd);
-%while (diffvar>1e-10) & (diffm>1e-10)
-while (diffvar>1e-7) & (diffm>1e-8)
+% plotbuffer(pd);
+% while (diffvar > 1e-10) & (diffm > 1e-10)
+while (diffvar > 1e-7) & (diffm > 1e-8)
   emstepb;	% A batchwise EM-step
-  diffm = sum(sum((m-oldm).*(m-oldm)));
-  diffvar =sum((var-oldvar).*(var-oldvar));
+  diffm = sum(sum((m - oldm).*(m - oldm)));
+  diffvar =sum((var - oldvar).*(var - oldvar));
   %plotrefresh(pd);
   pause(0.3);
 end;
